9a0e298cf05d5fa6adeeb3d838d06e58dc060124
ArianaGi <gir23002@byui.edu>
2026-01-21 12:44:52 -0700

    m

--- meetings/models.py
+++ meetings/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 
 class Meeting(models.Model):
-    # This stores the date.
-    date = models.DateField()
     
-    # This stores the hours
-    hours_worked = models.DecimalField(max_digits=5, decimal_places=2)
+    date = models.DateField() #store date.
     
-    # This stores the text description of the work.
-    description = models.TextField()
+    hours_worked = models.DecimalField(max_digits=5, decimal_places=2) #stores hours
+    
+    description = models.TextField() #store text description of the work.
 
     def __str__(self):
-        # This tells Django how to name a meeting in the admin list
-        return f"Meeting on {self.date}"
+        return f"Meeting on {self.date}" #tells Django how to name a meeting in the admin list
